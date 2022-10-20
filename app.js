9635e738a5ee9c8db5a4bd1914ab4ab96ee00482
softquakers <81610881+softquakers@users.noreply.github.com>
2022-10-20 10:00:27 +0530

    test

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
-
+app.get("/xxx", (req, res) => res.json({
+  message:"success"
+}));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
