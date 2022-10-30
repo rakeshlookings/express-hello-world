3780d650b80660cd3fdf900dd4309d2be4dc71b8
rakeshlookings <rakesh@lookings.in>
2022-10-31 02:00:21 +0530

    search filters

--- src/services/Ledger.service.js
+++ src/services/Ledger.service.js
@@ -1,28 +1,46 @@
 const Ledger = require('../models/Ledger.model')
 
-const create = async({body}) => {
-    const object =  new Ledger({
-        amount:body.amount, category: body.category, seller:body.seller, reference: body.reference, date:new Date()
+const create = async ({ body }) => {
+    const object = new Ledger({
+        amount: body.amount, category: body.category, seller: body.seller, reference: body.reference, date: new Date()
     })
     const obj = await object.save()
     return {
         status: true,
-        obj:obj
+        obj: obj
     }
 }
 
-const list = async({query}) => {
+const list = async ({ query }) => {
     let filter = {}
-    if (query?.timespan === 'day') {
-        let span = listByDaySpan()
-        span = new Date() - span
-        filter = {"date"  : {$gte : span}}
-        console.log(span)
-    }
+    let span = 0
     let page = 0; limit = 10
+    if (query?.timespan === 'day') {
+        span = listByDaySpan()
+    }
+    if (query?.timespan === 'week') {
+        span = listByWeekSpan()
+    }
+    if (query?.timespan === 'month') {
+        span = listByMonthSpan()
+    }
+    if (query?.timespan === 'custom' && query.start && query.end){
+        filter = {
+            $and: [{ "date": { $gt: query.start } }, { "date" : { $lte: query.end } }]
+          }
+    } else {
+        filter.date = { $gte: span }
+    }
+    
     if (query.page && query.limit) {
         page = query.page;
         limit = query.limit
+    }
+    if (query?.category){
+        filter.category = category
+    }
+    if (query?.search){
+        filter.seller =  { $regex: query?.search, $options: "i" }
     }
     let items = await Ledger.find(filter).skip(Number(page) * Number(limit)).limit(Number(limit))
     items = items.map(item => {
@@ -36,7 +54,7 @@ const list = async({query}) => {
         }
     })
     return {
-        status:true,
+        status: true,
         items: items
     }
 }
@@ -47,20 +65,39 @@ const listByDaySpan = () => {
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
     const span = ((hours * 3600) + (minutes * 60) + (seconds) ) * 1000
-    console.log(hours,minutes,seconds,span)
-    return span
+    return (date - span)
 }
 
-const getOne = async({params}) => {
-    const item = await Ledger.findOne({_id:params.id}).lean()
+const listByWeekSpan = () => {
+    const date = new Date();
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const day = date.getDay()
+    const span = ((day * 24 * 3600) + (hours * 3600) + (minutes * 60) + (seconds) )* 1000 
+    return (date - span)
+}
+
+const listByMonthSpan = () => {
+    const date = new Date();
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const monthDate = date.getDate()
+    const span = ((monthDate * 24 * 3600) + (hours * 3600) + (minutes * 60) + (seconds) )* 1000 
+    return (date - span)
+}
+
+const getOne = async ({ params }) => {
+    const item = await Ledger.findOne({ _id: params.id }).lean()
     return {
-        status:true,
+        status: true,
         items: item
     }
 }
 
 module.exports = {
-   create,
-   list,
-   getOne
+    create,
+    list,
+    getOne
 }
